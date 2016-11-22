edcf278ea9ad4ad374531a96c1f001ba4b0573a5
Axel claver <axel@axflash.com>
2016-11-22 14:50:44 +0100

    updated files

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'my-abckit',
+    modulePrefix: 'memorygame',
     environment: environment,
     rootURL: '/',
     locationType: 'auto',
